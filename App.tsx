9d0c312ac1b97dc011cc9c111e343c619da98718
Mike-U5 <maikelhaarmans2@gmail.com>
2019-10-15 09:28:19 +0200

    Fixed vulnerabilities in package.lock.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { ImageBackground, StyleSheet, View, Image } from 'react-native';
 import { StartMenuButton } from './components/StartMenuButton';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffc0c0',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
 });
 
@@ -16,10 +17,11 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
-      <Image style={{width: 150, height: 150}} source={require('./assets/icon.png')}/>
-      <StartMenuButton onPress={btnAction} text='Create Profile'/>
-      <StartMenuButton onPress={btnAction} text='One Time Use'/>
+    <ImageBackground source={require('./assets/gradient.png')} style={styles.container}>
+        <Image style={{width: 150, height: 150}} source={require('./assets/icon.png')}/>
+        <StartMenuButton onPress={btnAction} text='Create Profile'/>
+        <StartMenuButton onPress={btnAction} text='One Time Use'/>
+      </ImageBackground>
     </View>
   );
 }
--- components/StartMenuButton.tsx
+++ components/StartMenuButton.tsx
@@ -2,27 +2,29 @@ import React, {Component} from 'react';
 import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   button: {
     alignItems: 'center',
     backgroundColor: '#FFF',
     width: 250,
     height: 50,
-    borderRadius: 20,
+    borderRadius: 25,
+    marginTop: 15,
+    marginBottom: 15
   },
   buttonTxt: {
+     height: 50,
      alignItems: 'center',
      justifyContent: 'center',
-     color: '#FF4E4E'
+     textAlignVertical: 'center',
+     color: '#FF4E4E',
+     fontSize: 25
   }
 });
 
 export class StartMenuButton extends Component<{onPress: any, text: string}> {
   render() {
     return (
-     <View style={styles.container}>
+     <View>
       <TouchableOpacity style={styles.button} onPress={this.props.onPress}>
         <Text style={styles.buttonTxt}> {this.props.text} </Text>
       </TouchableOpacity>
