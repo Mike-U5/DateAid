5753e5f050fa3d736641bf9d9ecbaf04994eaea6
robintenhoven <robintenhoven19@gmail.com>
2019-10-15 10:50:03 +0200

    Made a form and added data to local storage. Still not buggfree and doesn't work

--- App.tsx
+++ App.tsx
@@ -1,25 +1,68 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image } from 'react-native';
-import { StartMenuButton } from './components/StartMenuButton';
+import {
+  StyleSheet, Text, View, TouchableOpacity, AsyncStorage, TextInput
+} from 'react-native';
 
+export default class App extends React.Component {
+  state = {
+      name: '',
+      preference: '',
+      age: ''
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Name</Text>
+        <TextInput
+          placeholder="Fill in your name here:"
+          onChangeText={(value) => this.setState({name: value})}
+          value={this.state.name}
+        />
+        <TextInput
+          onChangeText={(value) => this.setState({preference: value})}
+          placeholder="Fill in your preferences here:"
+          value={this.state.preference}
+        />
+        <TextInput
+          onChangeText={(value) => this.setState({age: value})}
+          placeholder="Fill in your age here:"
+          value={this.state.age}
+        />
+        <TouchableOpacity onPress={this.saveData}>
+          <Text> Click me to save date </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.displayData}>
+          <Text> Click me to show date </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  saveData = () => {
+    console.log(this.state);
+    /*let obj = {
+      name: "Robin ten Hoven",
+      preference: "sportiviteit",
+      age: 43
+    }*/
+    AsyncStorage.setItem('data', JSON.stringify(this.state));
+  }
+  displayData = async() => {
+    try {
+      let user = await AsyncStorage.getItem('data');
+      let parsed = JSON.parse(user);
+      alert(parsed.name + " " + parsed.preference + " " + parsed.age);
+    }
+    catch(error) {
+      alert(error);
+    }
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffc0c0',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default function App() {
-  const btnAction = function() {alert('You tapped the button!')};
-
-  return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-      <Image style={{width: 150, height: 150}} source={require('./assets/icon.png')}/>
-      <StartMenuButton onPress={btnAction} text='Create Profile'/>
-      <StartMenuButton onPress={btnAction} text='One Time Use'/>
-    </View>
-  );
-}
+    justifyContent: 'center'
+  }
+})
