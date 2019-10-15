c8b119bd2dcd0e264497f29e6123e8f0e35da66b
Mike-U5 <maikelhaarmans2@gmail.com>
2019-10-15 10:48:45 +0200

    Fixed tabbing and added Colors Enum.

--- components/StartMenuButton.tsx
+++ components/StartMenuButton.tsx
@@ -1,24 +1,25 @@
 import React, {Component} from 'react';
 import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import { Colors } from '../enums/Colors';
 
 const styles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-    width: 250,
-    height: 50,
-    borderRadius: 25,
-    marginTop: 15,
-    marginBottom: 15
-  },
-  buttonTxt: {
-     height: 50,
-     alignItems: 'center',
-     justifyContent: 'center',
-     textAlignVertical: 'center',
-     color: '#FF4E4E',
-     fontSize: 25
-  }
+	button: {
+		alignItems: 'center',
+		backgroundColor: '#FFF',
+		width: 250,
+		height: 50,
+		borderRadius: 25,
+		marginTop: 15,
+		marginBottom: 15
+	},
+	buttonTxt: {
+		height: 50,
+		alignItems: 'center',
+		justifyContent: 'center',
+		textAlignVertical: 'center',
+		color: Colors.BgDark,
+		fontSize: 25
+	}
 });
 
 export class StartMenuButton extends Component<{onPress: any, text: string}> {
---
+++ enums/Colors.ts
@@ -0,0 +1,8 @@
+export enum Colors {
+	BgLight = '#FFC0C0',
+	BgDark = '#FF6060',
+	LogoBright = '#FF0707',
+	LogoMed = '#991D1D',
+	LogoDark = '#333333',
+	White = '#FFFFFF'
+}
