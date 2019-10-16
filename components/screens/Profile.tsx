c2d273fb2cdc71736e7fbc6bbf61dea9eebf4225
Antvall <timo_timmermans@hotmail.com>
2019-10-16 10:30:03 +0200

    Screens added

--- components/screens/Profile.tsx
+++ components/screens/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from "react-native";
 
-export class Loading extends Component<{name: string}> {
+export class Profile extends Component<{name: string}> {
 	render() {
 		return (
 			<View style={{alignItems: 'center'}}>
