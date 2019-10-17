d6cebf536f21f75b27ef02ae5b38376a14c67e50
robintenhoven <robintenhoven19@gmail.com>
2019-10-17 12:08:47 +0200

    Duplicates not allowed, Removes on duplicate, now saved as an array

--- components/Example1.tsx
+++ components/Example1.tsx
@@ -6,34 +6,44 @@ class Example1 extends Component {
 	render() {
 		return (
 					<TouchableOpacity>
+					<Text onPress={() => this.wipeSession()}>	WIPE SESSION </Text>
 						{interestData.map(s =>
 							(<Text
 								key={s.name}
-								onPress={() => this.saveInterest(s.name)}>{s.name}
+								onPress={() => this.saveInterest(this, s.id, s.name)}>{s.name}
 							</Text>)
 						)}
 					</TouchableOpacity>
         );
     }
-		saveInterest = (interest) => {
-			//get getItem
-			//if exists, delete item
-			//if doesn't exist, save
-			//inArray
-			
-			AsyncStorage.getItem(interest).then((data) => {
-				if(data === null) {
-					console.log(interest + " stond er nog niet in.")
-					AsyncStorage.setItem(interest, "1");
+
+		wipeSession = () => {
+			AsyncStorage.clear();
+		}
+		saveInterest = (element, id, interest) => {
+			AsyncStorage.getItem("interests").then(async (data) => {
+				console.log(data);
+				//if session array doesn't exist yet, add empty array
+				if(data === null || data === undefined || data === "0") {
+					AsyncStorage.setItem("interests", JSON.stringify([id]));
 				} else {
-			    if (data !== null && data !== undefined && data == "1") {
-						console.log(interest + " stond wel in local storage, nu naar 0");
-						AsyncStorage.setItem(interest, "0");
+					//check if array already contains id, true: remove from array and update, false: add to array and update
+					let tempArray = JSON.parse(data);
+					console.log(tempArray);
+					if(tempArray.includes(id)) {
+						var index = tempArray.indexOf(id);
+						if (index > -1) { //Make sure item is present in the array, without if condition, -n indexes will be considered from the end of the array.
+						  tempArray.splice(index, 1);
+							AsyncStorage.setItem("interests", JSON.stringify(tempArray));
+						}
 					} else {
-						console.log(interest + " stond niet in local storage, nu naar 1")
-						//perform save
-						AsyncStorage.setItem(interest, "1");
+						tempArray.push(id);
+						AsyncStorage.setItem("interests", JSON.stringify(tempArray));
 					}
+
+					AsyncStorage.getItem("interests").then(async (data2) => {
+						console.log(data2);
+					});
 				}
 			});
 		}
