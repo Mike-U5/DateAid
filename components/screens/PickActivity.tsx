49c732e4afc4fd7a1af904a67c228e13b3b3f353
Antvall <timo_timmermans@hotmail.com>
2019-11-08 11:41:24 +0100

    NAvigation Arrow fix

--- components/screens/PickActivity.tsx
+++ components/screens/PickActivity.tsx
@@ -4,7 +4,7 @@ import { View, Text, Image, TouchableHighlight } from 'react-native';
 export class PickActivity extends Component<{name: string, navigation: any}> {
 	static navigationOptions = ({ navigation }) => ({
 		headerLeft: (
-			<TouchableHighlight onPress={() => navigation.goBack()} style={{width: 20, height: 20, marginStart: 25}}>
+			<TouchableHighlight onPress={() => navigation.goBack()} style={{width: 40, height: 40, marginStart: 25, padding: 10}}>
 				<Image source={require('../../assets/material/left-arrow.png')} style={{width: 20, height: 20, tintColor: '#fff'}}	/>
 			</TouchableHighlight>
 		),
--- components/screens/SetInterests.tsx
+++ components/screens/SetInterests.tsx
@@ -5,12 +5,12 @@ import { View, Image, TouchableHighlight } from 'react-native';
 export class SetInterests extends Component<{name: string, navigation: any}> {
 static navigationOptions = ({ navigation }) => ({
 	headerRight: (
-		<TouchableHighlight onPress={() => navigation.navigate('PickActivity')} style={{width: 20, height: 20, marginEnd: 25}}>
+		<TouchableHighlight onPress={() => navigation.navigate('PickActivity')} style={{width: 40, height: 40, marginEnd: 25, padding: 10}}>
 			<Image source={require('../../assets/material/check.png')} style={{width: 20, height: 20, tintColor: '#fff'}}	/>
 		</TouchableHighlight>
 	),
 	headerLeft: (
-		<TouchableHighlight onPress={() => navigation.goBack()} style={{width: 20, height: 20, marginStart: 25}}>
+		<TouchableHighlight onPress={() => navigation.goBack()} style={{width: 40, height: 40, marginStart: 25, padding: 10}}>
 			<Image source={require('../../assets/material/left-arrow.png')} style={{width: 20, height: 20, tintColor: '#fff'}}	/>
 		</TouchableHighlight>
 	),
