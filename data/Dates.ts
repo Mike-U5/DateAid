05b91725be1b1b26b03d642d3624a2e3464765a7
Antvall <timo_timmermans@hotmail.com>
2019-11-08 12:35:12 +0100

    Activity images

--- data/Dates.ts
+++ data/Dates.ts
@@ -1,9 +1,11 @@
 const Dates: Array<{id: number, name: string, type: Array<number>, interests: Array<number>}> = [
-	{id: 0, name: 'dateName', type: [1, 2, 3], interests: [11]},
+	{id: 0, name: 'Cinema', type: [1, 2, 3], interests: [11]},
 	{id: 1, name: 'Bowling', type: [1, 2, 3], interests: [4]},
 	{id: 2, name: 'Escape Room', type: [1, 2, 3], interests: []},
 	{id: 3, name: 'Hiking', type: [1, 2, 3], interests: [4]},
 	{id: 4, name: 'Zoo', type: [1, 2, 3], interests: [9]},
 	{id: 5, name: 'Movies', type: [1, 2, 3], interests: [8]},
-	{id: 6, name: 'Diner', type: [1, 2, 3], interests: [10]}
+	{id: 6, name: 'Dinner', type: [1, 2, 3], interests: [10]}
 ]
+
+export default Dates;
