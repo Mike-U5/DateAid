3836c7868f8abc83143e683ab032abd50960a85f
Mike-U5 <maikelhaarmans2@gmail.com>
2019-11-21 11:30:21 +0100

    Updated navHeader.

--- components/features/NavHeader.tsx
+++ components/features/NavHeader.tsx
@@ -3,21 +3,29 @@ import { Image, View, TouchableHighlight, ImageSourcePropType } from 'react-nati
 import { Colors } from '../../enums/Colors';
 
 const style = {
-	txt: {
-		fontSize: 40,
-		color: Colors.IkkonzomePink
+	touch: {
+		width: 40,
+		height: 40,
+		marginEnd: 25,
+		padding: 10
+	},
+	img: {
+		width: 20,
+		height: 20,
+		tintColor: Colors.White
 	}
 }
 
-export class NavHeader extends Component<{imgSrc: ImageSourcePropType, navigationDestination: any}> {
+export class NavHeader extends Component<{imgSrc: ImageSourcePropType, navDest: any}> {
 
 	render() {
 		return (
 			<View>
-				<TouchableHighlight onPress={() => this.props.navigationDestination} style={{width: 40, height: 40, marginEnd: 25, padding: 10}}>
-					<Image source={this.props.imgSrc} style={{width: 20, height: 20, tintColor: Colors.White}}	/>
+				<TouchableHighlight onPress={() => this.props.navDest} style={style.touch}>
+					<Image source={this.props.imgSrc} style={style.img}	/>
 				</TouchableHighlight>
 			</View>
 		);
 	}
+	
 }
