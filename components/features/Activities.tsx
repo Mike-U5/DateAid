9a11175a01b7f64da3246f5e4bb31244bddd65ed
Antvall <timo_timmermans@hotmail.com>
2019-11-26 10:37:31 +0100

    Page Number

--- components/features/Activities.tsx
+++ components/features/Activities.tsx
@@ -9,20 +9,22 @@ import { DateHelper } from '../../helpers/DateHelper';
 const screenWidth = Math.round(Dimensions.get('window').width) * 0.95;
 const screenHeight = Math.round(Dimensions.get('window').height) * 0.9;
 
-class Activities extends Component<{navigation: any}, {sliceNum1: number, sliceNum2: number, arrayNum: number, buttonText: string, isReady: boolean}> {
+class Activities extends Component<{navigation: any}, {sliceNum1: number, sliceNum2: number, arrayNum: number, buttonText: string, isReady: boolean, totalPages: number, currentPage: number}> {
 	private matchingDates: DateItem[] = [];
 
 	constructor(props: Readonly<{ navigation: any; }>) {
 		super(props);
-		this.state = {sliceNum1: 0, sliceNum2: 3, arrayNum: 0, buttonText: 'Load more', isReady: false};
+		this.state = {sliceNum1: 0, sliceNum2: 3, arrayNum: 0, buttonText: 'Load more', isReady: false, totalPages: 0, currentPage: 1};
 
 		TempStorage.userInterests.get().then((userInterests) => {
 			this.matchingDates = DateHelper.getRelevantDates(userInterests);
 			this.setState({isReady: true, arrayNum: this.matchingDates.length});
+			this.generateTotalPageNumber();
 		});
 	}
 
 	render() {
+
 		if (!this.state.isReady) {
 			return(<View><Text>Generating dates...</Text></View>);
 		} else if (this.matchingDates.length === 0) {
@@ -38,6 +40,7 @@ class Activities extends Component<{navigation: any}, {sliceNum1: number, sliceN
 					<View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
 					{this.generateRealContent()}
 					</View>
+					<Text>{this.state.currentPage} / {this.state.totalPages}</Text>
 					<LoadActivitiesButton onPress={() => this.loadActivities()} text={this.state.buttonText} />
 					</ScrollView>
 				);
@@ -90,11 +93,33 @@ class Activities extends Component<{navigation: any}, {sliceNum1: number, sliceN
 			}
 			this.generateButtonText();
 			this.setState({sliceNum1: number1, sliceNum2: number2});
+			this.generateCurrentPageNumber();
 		}
 
 		generateButtonText = () => {
 			if ((this.state.sliceNum2 === (this.state.arrayNum - 1)) || this.state.sliceNum2 === (this.state.arrayNum - 2) || (this.state.sliceNum2 === this.state.arrayNum - 3))
 			{this.setState({buttonText: 'Back to first'}); }else{this.setState({buttonText: 'Load more...'}); }
+		}
+
+		generateTotalPageNumber = () => {
+			const arrayLength = this.state.arrayNum;
+			const totalPages = Math.ceil(arrayLength / 3);
+			console.log(arrayLength);
+			console.log(totalPages);
+
+			this.setState({totalPages: (Number(totalPages))});
+		}
+
+		generateCurrentPageNumber = () => {
+			const totalPages = this.state.totalPages;
+			let currentPage = this.state.currentPage;
+
+			if (currentPage === totalPages){
+				currentPage = 1;
+			} else if (currentPage < totalPages) {
+				currentPage++;
+			}
+			this.setState({currentPage: currentPage});
 		}
 
 }
