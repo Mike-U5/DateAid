764e32002aaa9a33d0925bb450d7864673c17047
Mike-U5 <maikelhaarmans2@gmail.com>
2019-12-05 10:30:19 +0100

    Removed dateTypeButton.

--- components/screens/SelectDateType.tsx
+++ components/screens/SelectDateType.tsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import { View, Dimensions } from 'react-native';
+import { View } from 'react-native';
 import { NavHelper } from '../../helpers/NavHelper';
 import { NavIcons } from '../../data/NavIcons';
 import DateTypes from '../../data/DateTypes';
-import { DateTypeButton } from '../features/DateTypeButton';
 import { TempStorage } from '../../helpers/TempStorage';
-
-const screenWidth = Math.round(Dimensions.get('window').width);
+import { RectangleImageButton } from '../elements/RectangleImageButton';
 
 export class SelectDateType extends Component<{navigation: { navigate: (a1: string) => any; }}>  {
 	/** Navigation for this page **/
@@ -22,11 +20,12 @@ export class SelectDateType extends Component<{navigation: { navigate: (a1: stri
 	private renderButtons() {
 		const buttons = [];
 		for (let i = 0; i < DateTypes.length; i++) {
-			const dt = DateTypes[i];
-			if (i !== 0) {
-				buttons.push(<View key={'temp' + i} style={{width: (screenWidth / 20)}}/>);
-			}
-			buttons.push(<DateTypeButton key={i} onPress={() => this.onPressBtn(i)} dateType={dt} />);
+			buttons.push(<RectangleImageButton
+				key={i}
+				onPress={() => this.onPressBtn(i)}
+				name={DateTypes[i].name}
+				src={DateTypes[i].src}
+			/>);
 		}
 		return buttons;
 	}
