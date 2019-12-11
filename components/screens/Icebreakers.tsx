63fc938416dfdf813d8a7e3ebecdd366dcdee76f
robintenhoven <robintenhoven19@gmail.com>
2019-12-11 12:56:37 +0100

    icebreakers

--- components/screens/Icebreakers.tsx
+++ components/screens/Icebreakers.tsx
@@ -5,7 +5,44 @@ export class Icebreakers extends Component<{name: string, navigation: any}> {
 	render() {
 		return (
 			<View style={{alignItems: 'center'}}>
-				<Text> Hallo </Text>
+				<Text>Are you a cat or a dog person? </Text>
+				<Text>Did you go to any concerts lately, if yes, which one? </Text>
+				<Text>What if you got free from work/school tomorrow, what would you do? </Text>
+				<Text>Who is your role model? </Text>
+				<Text>What do you love the most? </Text>
+				<Text>What is the best vacation you ever took? </Text>
+				<Text>What is the most inspirational thing you read lately? </Text>
+				<Text>Ask him/her about something she is wearing. </Text>
+				<Text>Ask him/her what is on his/her bucketlist. </Text>
+				<Text>Ask him/her for 2 truths and one lie! </Text>
+				<Text>How many siblings do you have? </Text>
+				<Text>Where did you grow up? </Text>
+				<Text>What were some of your favorite things growing up?</Text>
+				<Text>What’s your favorite childhood memory and why?</Text>
+				<Text>Did you have any pets when you were little? What were their names and what was it like to have them?</Text>
+				<Text>What’s the scariest thing you ever encountered as a kid?</Text>
+				<Text>What school did you go to?</Text>
+				<Text>Do you have any childhood friends that ended up being lifelong friends? Who were they and what are they like?</Text>
+				<Text>What were your parents like when you were growing up?</Text>
+				<Text>What was your family’s favorite thing to do on weekends? </Text>
+				<Text>Where do you work and how long have you been working with them?</Text>
+				<Text>Can you describe your usual work day?</Text>
+				<Text>If there’s one thing you can change about the nature of your job, what would it be and why?</Text>
+				<Text>What’s your favorite thing about the work that you do?</Text>
+				<Text>Are you the early bird or are you typically the last one in?</Text>
+				<Text>What do you like about your boss? What do you dislike about them?</Text>
+				<Text>What do you like to do for fun after a busy work day?</Text>
+				<Text>Who are your work friends? What do you like about them?</Text>
+				<Text>If you could have things your way, where would you be in terms of your job 10 years from now?</Text>
+				<Text>If you could have an entire day to yourself, what would you do?</Text>
+				<Text>If you could take the whole month off, how would you spend your time?</Text>
+				<Text>What do you like to do for fun?</Text>
+				<Text>What are the weekends like for you?</Text>
+				<Text>Are there any advocacies or groups that you’re a part of? What are they?</Text>
+				<Text>What’s one thing you wish you could spend more time doing?</Text>
+				<Text>If you could take up a new hobby, what would it be and why?</Text>
+				<Text>What one talent do you wish you could have?</Text>
+				<Text>If you could become a master of something, what would that something be and why?</Text>
 			</View>
 		);
 	}
