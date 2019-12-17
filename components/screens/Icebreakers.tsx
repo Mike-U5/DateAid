77337dec02473c77b3897095ad4f80d2f93b4900
robintenhoven <robintenhoven19@gmail.com>
2019-12-17 11:33:45 +0100

    comments added, dropdown working, icebreakers almost done.

--- components/screens/Icebreakers.tsx
+++ components/screens/Icebreakers.tsx
@@ -1,91 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
+import { ScrollView, Text, Picker} from 'react-native';
+import icebreakers, {IcebreakerCategory} from '../../data/Icebreakers';
+export class Icebreakers extends Component<{name: string, navigation: any}, {pickerSelection: any, icebreakers: any}> {
+	
+	constructor(props: Readonly<{ name: string; navigation: any; }>) {
+		super(props);
+		this.state = { pickerSelection: '', icebreakers: 0}
+	}
 
-export class Icebreakers extends Component<{name: string, navigation: any}> {
 	render() {
-		return (
-			<View style={{alignItems: 'center'}}>
-				<Text>Are you a cat or a dog person? </Text>
-				<Text>Did you go to any concerts lately, if yes, which one? </Text>
-				<Text>What if you got free from work/school tomorrow, what would you do? </Text>
-				<Text>Who is your role model? </Text>
-				<Text>What do you love the most? </Text>
-				<Text>What is the best vacation you ever took? </Text>
-				<Text>What is the most inspirational thing you read lately? </Text>
-				<Text>Ask him/her about something she is wearing. </Text>
-				<Text>Ask him/her what is on his/her bucketlist. </Text>
-				<Text>Ask him/her for 2 truths and one lie! </Text>
-				<Text>How many siblings do you have? </Text>
-				<Text>Where did you grow up? </Text>
-				<Text>What were some of your favorite things growing up?</Text>
-				<Text>What’s your favorite childhood memory and why?</Text>
-				<Text>Did you have any pets when you were little? What were their names and what was it like to have them?</Text>
-				<Text>What’s the scariest thing you ever encountered as a kid?</Text>
-				<Text>What school did you go to?</Text>
-				<Text>Do you have any childhood friends that ended up being lifelong friends? Who were they and what are they like?</Text>
-				<Text>What were your parents like when you were growing up?</Text>
-				<Text>What was your family’s favorite thing to do on weekends? </Text>
-				<Text>Where do you work and how long have you been working with them?</Text>
-				<Text>Can you describe your usual work day?</Text>
-				<Text>If there’s one thing you can change about the nature of your job, what would it be and why?</Text>
-				<Text>What’s your favorite thing about the work that you do?</Text>
-				<Text>Are you the early bird or are you typically the last one in?</Text>
-				<Text>What do you like about your boss? What do you dislike about them?</Text>
-				<Text>What do you like to do for fun after a busy work day?</Text>
-				<Text>Who are your work friends? What do you like about them?</Text>
-				<Text>If you could have things your way, where would you be in terms of your job 10 years from now?</Text>
-				<Text>If you could have an entire day to yourself, what would you do?</Text>
-				<Text>If you could take the whole month off, how would you spend your time?</Text>
-				<Text>What do you like to do for fun?</Text>
-				<Text>What are the weekends like for you?</Text>
-				<Text>Are there any advocacies or groups that you’re a part of? What are they?</Text>
-				<Text>What’s one thing you wish you could spend more time doing?</Text>
-				<Text>If you could take up a new hobby, what would it be and why?</Text>
-				<Text>What one talent do you wish you could have?</Text>
-				<Text>If you could become a master of something, what would that something be and why?</Text>
-				<Text>Where do you see yourself living 10 years from now?</Text>
-				<Text>Do you think you’d be the kind of person who would be happy to settle down or is that something that doesn’t fully appeal to you?</Text>
-				<Text>How do you envision yourself spending your senior years?</Text>
-				<Text>Kids – yes or no? How many children do you think you’d want to have when you’re ready?</Text>
-				<Text>A one-month cruise to a tropical destination or a long vacation in the Alps?</Text>
-				<Text>Coffee or tea?</Text>
-				<Text>Breakfast or dinner?</Text>
-				<Text>Read a book indoors or spend aa night out with friends?</Text>
-				<Text>Wine and dine or Netflix and chill?</Text>
-				<Text>Comfy casual or fashionable and formal?</Text>
-				<Text>Dress shoes or comfy sneakers?</Text>
-				<Text>Sports or lounging?</Text>
-				<Text>A trip to the gym or a trip to a new restaurant?</Text>
-				<Text>Japanese food or Italian?</Text>
-				<Text>Morning or night?</Text>
-				<Text>Cats or dogs?</Text>
-				<Text>Rain or sun?</Text>
-				<Text>Go on a hike or go bar hopping?</Text>
-				<Text>Outdoors or indoors?</Text>
-				<Text>Movies or TV shows?</Text>
-				<Text>What’s one vacation trip that you’ll never forget? What was it about the experience that makes it unforgettable?</Text>
-				<Text>When’s the last time you did something for the first time? What was it and how did you feel about it?</Text>
-				<Text>When you’re feeling shy and uncertain, what do you usually do?</Text>
-				<Text>How did you meet your best friend and why do you think your relationship has worked out so well?</Text>
-				<Text>Did you ever have to cut someone out of your life? What happened, if you don’t mind sharing?</Text>
-				<Text>If you could live anywhere in the world, where would you move and why?</Text>
-				<Text>What are you looking for in a partner? Do you have any clue as to why your previous relationships didn’t work out?</Text>
-				<Text>What’s the most expensive thing you’ve ever bought for yourself?</Text>
-				<Text>What’s one piece of important advice that you think everyone should know?</Text>
-				<Text>What’s your favorite place in the whole world and why?</Text>
-				<Text>Describe your perfect day.</Text>
-				<Text>What accomplishments are you really proud of?</Text>
-				<Text>If you had all the money in the world, what would be the first three things you’d buy and why?</Text>
-				<Text>If you won the lottery, name 10 things you would spend your new funds on.</Text>
-				<Text>What’s the worst experience you’ve ever had on a date?</Text>
-				<Text>What is your go-to movie or TV show if you just can’t find anything else that you’d like to watch?</Text>
-				<Text>What are some of the little things that make you smile or that brighten your day?</Text>
-				<Text>If you could tell your 10 year old self one thing, what would it be and why?</Text>
-				<Text>What kind of music do you like to listen to?</Text>
-				<Text>What’s your favorite memory of your childhood?</Text>
-				<Text>What do you like to do over the holidays?</Text>
-				<Text>What do you think the world will look like 50 years from now?</Text>
-			</View>
-		);
+			return (
+				<ScrollView>
+				<Text> The Default value is {this.state.pickerSelection} </Text>
+					<Picker
+						selectedValue = {this.state.pickerSelection}
+						onValueChange={(itemValue, itemIndex) =>
+							this.filterContent(itemValue, itemIndex)
+						}>
+						{this.renderPickerItems()}
+					</Picker>
+					{this.renderIcebreakers()}
+				</ScrollView>
+			);
+	}
+
+	/* handler that executes on dropdown button change */
+	private filterContent = (pickerValue: any, pickerIndex: any) => {
+		this.setState({pickerSelection: pickerValue, icebreakers: pickerIndex});
+		this.renderIcebreakers();
+	}
+
+	/* generates the dropdown items based on the Enum IcebreakerCategory */
+	private renderPickerItems = () => {
+		const pickerItems: JSX.Element[] = [];
+		for (let i = 0; i < Object.keys(IcebreakerCategory).length / 2; i++) {
+			const s = IcebreakerCategory[i];
+			pickerItems.push(<Picker.Item key={s} label={s} value={s}/>);
+		}
+		return pickerItems
+	}
+
+	/* 	function that gets executed when the filter changes and shows icebreakers on the screen	 */
+	private renderIcebreakers = () => {
+		const icebreakerValue: JSX.Element[] = [];
+		if (this.state.icebreakers === IcebreakerCategory.All) {
+			for (let i = 0; i < icebreakers.length; i++) {
+				const s = icebreakers[i];
+				icebreakerValue.push(<Text key={s.text}> {s.text} </Text>);
+			}
+		} else {
+			for (let i = 0; i < icebreakers.length; i++) {
+				const s = icebreakers[i];
+				if (s.category === this.state.icebreakers) {
+					icebreakerValue.push(<Text key={s.text}> {s.text} </Text>);
+				}
+			}
+		}
+		return icebreakerValue
 	}
 }
